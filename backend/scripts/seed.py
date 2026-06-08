edb53152df50d138dc9357c2b0dfbc7a5e910d9d
RuslanVoskresenskyi <vos.nau.04@gmail.com>
2026-06-08 10:04:42 +0300

    add postgres volume and seed script

---
+++ backend/scripts/seed.py
@@ -0,0 +1,44 @@
+import psycopg2
+import random
+from datetime import datetime
+
+conn = psycopg2.connect(
+  host="localhost",
+  port=3005,
+  dbname="events_db",
+  user="admin",
+  password="password"
+)
+
+cursor = conn.cursor()
+print("Connected!")
+
+events = ["click", "login", "search", "purchase", "logout"]
+
+USER_COUNT = 10000
+
+try:
+  for i in range(USER_COUNT):
+    username = f"user_{i + 1}"
+    cursor.execute("INSERT INTO users (username) VALUES (%s) RETURNING id", (username,))
+    user_id = cursor.fetchone()[0]
+    events_count = random.randint(3, 7)
+    for j in range(events_count):
+      event_name = random.choice(events)
+      occurred_at = datetime.now()
+      cursor.execute(
+        "INSERT INTO events (user_id, event_name, occurred_at) VALUES  (%s, %s, %s)",
+        (user_id, event_name, occurred_at)
+      )
+
+  conn.commit()
+  print("Done!")
+except Exception as e:
+  conn.rollback()
+  print(f"Error: {e}")
+finally:
+  cursor.close()
+  conn.close()
+
+
+
