5ff84fb7e0dc9920ab554f615e8a27d4cd54bef5
RuslanVoskresenskyi <vos.nau.04@gmail.com>
2026-06-14 17:16:05 +0300

    Add CORS middleware for local development

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 from schema import schema
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 graphql_app = GraphQLRouter(schema)
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["http://localhost:3000"],
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 app.include_router(graphql_app, prefix="/graphql")
