a03d043fbec816da08c7904b8cedfe4ca44fc6be
RuslanVoskresenskyi <vos.nau.04@gmail.com>
2026-06-21 14:05:11 +0300

    Add schemas EventPerDay and EventPerType

--- backend/schema.py
+++ backend/schema.py
@@ -1,10 +1,21 @@
 import strawberry
 from database import conn_pool
+from datetime import date
 
 @strawberry.type
 class DashboardSummary:
   total_events: int
   total_users: int
+
+@strawberry.type
+class EventPerDay:
+  date: date
+  count: int
+
+@strawberry.type
+class EventPerType:
+  event_name: str
+  count: int
 
 @strawberry.type
 class Query:
@@ -22,5 +33,27 @@ class Query:
       total_events=total_events,
       total_users=total_users
     )
+  @strawberry.field
+  def events_per_day(self) -> list[EventPerDay]:
+    conn = conn_pool.getconn()
+    cursor = conn.cursor()
+    cursor.execute("SELECT DATE(occurred_at), COUNT(*) FROM events GROUP BY DATE(occurred_at) ORDER BY DATE(occurred_at)")
+    rows = cursor.fetchall()
+    conn_pool.putconn(conn)
+
+    return [
+      EventPerDay(date=row[0], count=row[1]) for row in rows
+    ]
+  @strawberry.field
+  def events_per_type(self) -> list[EventPerType]:
+    conn = conn_pool.getconn()
+    cursor = conn.cursor()
+    cursor.execute("SELECT event_name, COUNT(*) FROM events GROUP BY event_name")
+    rows = cursor.fetchall()
+    conn_pool.putconn(conn)
+
+    return [
+      EventPerType(event_name=row[0], count=row[1]) for row in rows
+    ]
 
 schema = strawberry.Schema(query=Query)
